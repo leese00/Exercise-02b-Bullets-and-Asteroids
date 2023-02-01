d02a19b9b3e024edda49ddefad794d697a81ef5e
leese00 <122573963+leese00@users.noreply.github.com>
2023-02-01 00:23:33 -0500

    Completes exercise

---
+++ Asteroid/Asteroid.gd
@@ -0,0 +1,14 @@
+extends KinematicBody2D
+
+var velocity = Vector2.ZERO
+var initial_speed = 3.0
+
+func _ready():
+	velocity = Vector2(0,initial_speed*randf()).rotated(PI*2*randf())
+	pass
+	
+func _physics_process(_delta):
+		position = position + velocity
+		position.x + wrapf(velocity.x, 0, 1024)
+		position.y + wrapf(velocity.y, 0, 600)
+		
--- Global.gd
+++ Global.gd
@@ -1,5 +1,7 @@
 extends Node
 
+func _ready():
+	randomize()
 func _unhandled_input(event):
 	if event.is_action_pressed("menu"):
 		get_tree().quit()
